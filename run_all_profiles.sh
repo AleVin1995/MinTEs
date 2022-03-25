a10e6d3dcd085ef5ce94b75142e11f2eb000e867
Alessandro Vinceti (ext) <alessandro.vinceti@hnode02.cm.cluster>
2022-03-25 14:59:04 +0100

    Setup profile to run snakemake on slurm cluster
    - Integrated cookiecutter in conda env
    - Set up profile with default config.yaml to manage resources

--- run_all_profiles.sh
+++ run_all_profiles.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=main
 #SBATCH --partition=cpuq
 #SBATCH --mail-type=NONE
-#SBATCH --time=00:10:00
+#SBATCH --time=00:20:00
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --chdir=/scratch/alessandro.vinceti/Reduced_Templates
@@ -13,5 +13,12 @@ set -o nounset
 set -o pipefail
 
 echo 'Download sgRNA-level fold-change datasets'
-snakemake -s workflows/data_download/Snakefile --profile config/config.yml
+snakemake -s workflows/data_download/Snakefile --profile profile --jobs 1
 echo 'Finished downloading data'
+
+echo 'Data preprocessing'
+echo 'Cell-wise splitting to be used as BAGEL input'
+snakemake -s workflows/data_preprocessing/Snakefile --profile profile --jobs 1
+echo 'Single-cell files created'
+
+rm *out
