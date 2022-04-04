046020b11e3adaa9039d256e9108c5d134b596ae
alessandro.vinceti <alessandro.vinceti@fht.org>
2022-04-04 22:15:42 +0200

    Miscellanea
    - Changed check in run_all script to assemble datasets with the exact
      nº of columns/cell lines.
    - Corrected snakefile in the data preprocessing step.

--- run_all.sh
+++ run_all.sh
@@ -12,29 +12,43 @@ source ~/.bashrc
 conda activate MinTEs
 
 echo 'Download sgRNA-level fold-change datasets'
-snakemake -s workflows/data_download/Snakefile --profile profile
+snakemake -s workflows/data_download/snakefile --profile workflows/data_download/config
 echo 'Finished downloading data'
 
 echo 'Data preprocessing'
 for dataset in "Achilles" "Score"
 do
-    echo $dataset > project.txt
-    snakemake -s workflows/data_preprocessing/Snakefile --profile profile
+    N_cells_FC=$(2*$((head -1 resources/FC/Project_"$dataset"_corrected_FC.tsv | wc -l)))
+    N_cells_BF=$(head -1 resources/BF/*/Project_"$dataset"_BF.tsv | wc -l)
 
-    N_datasets=$(ls resources/BF/*/Project_"$dataset"_BF.tsv | wc -l)
-    N_jobs_assemble=$(squeue | grep assemble | wc -l)
-
-    while [[ $N_datasets -lt 2 && $N_jobs_assemble -eq 0 ]]
+    while [[ $N_cells_FC -gt $N_cells_BF ]]
     do
+        echo $dataset > project.txt
+        snakemake -s workflows/data_preprocessing/snakefile --profile workflows/data_preprocessing/config
+
+        N_datasets=0
+        N_jobs_assemble=0
+
+        while [[ $N_datasets -lt 2 && $N_jobs_assemble -eq 0 ]]
+        do
+            sleep 30
+            N_datasets=$(ls resources/BF/*/ | grep $dataset | grep tsv | wc -l)
+            N_jobs_assemble=$(squeue | grep assemble | wc -l)
+        done
+
         sleep 30
-        N_datasets=$(ls resources/BF/*/Project_"$dataset"_BF.tsv | wc -l)
-        N_jobs_assemble=$(squeue | grep assemble | wc -l)
+        N_cells_BF=$(head -1 resources/BF/*/Project_"$dataset"_BF.tsv | wc -l)
+        
+        if [[ $N_cells_FC -gt $N_cells_BF ]]
+        then
+            rm resources/BF/*/Project_"$dataset"_BF.tsv
+        fi
     done
 done
 
 rm project.txt
 echo 'Assembling and scaling of sgRNA-level Bayes factor datasets'
 
-rm slurm*
-rm -r .snakemake/
-rm -r log/
+#rm slurm*
+#rm -r .snakemake/
+#rm -r log/
